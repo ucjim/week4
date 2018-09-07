36369d44c7f2a9f0905f0d1f9b427ae6a220391b
jarudat chaikuad <60030014@kmitl.ac.th>
2018-09-07 14:26:17 +0700

    add 06

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,25 +5,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("00000000011111111112");
+            int n = 123456789;
 
-            Console.WriteLine("12345678901234567890");
+            Console.WriteLine("{0, 0:E}", n);
 
-            Console.WriteLine("{0, 0}", 1);
+            Console.WriteLine("{0, 0:F}", n);
 
-            Console.WriteLine("{0, 1}", 1);
+            Console.WriteLine("{0, 0:G}", n);
 
-            Console.WriteLine("{0, 2}", 1);
+            Console.WriteLine("{0, 0:N}", n);
 
-            Console.WriteLine("{0, 3}", 1);
+            Console.WriteLine("{0, 0:P}", n);
 
-            Console.WriteLine("{0, 5}", 1);
-
-            Console.WriteLine("{0, 10}", 1);
-
-            Console.WriteLine("{0, 15}", 1);
-
-            Console.WriteLine("{0, 20}", 1);
+            Console.WriteLine("{0, 0:X}", n);
             Console.ReadKey();
         }
     }
