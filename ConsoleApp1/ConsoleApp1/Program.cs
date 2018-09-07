d0b14a07e9a71bed2cd18dc8aaca554de50cfbcf
jarudat chaikuad <60030014@kmitl.ac.th>
2018-09-07 14:01:21 +0700

    W04P02

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" {0} and {1}", 3, 6);
+            Console.ReadKey();
+        }
+    }
+}
