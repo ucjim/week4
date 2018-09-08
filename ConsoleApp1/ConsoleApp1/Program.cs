793c880ffaf2ddcd4d98c92fd1348039df9334e6
jarudat chaikuad <60030014@kmitl.ac.th>
2018-09-09 02:25:21 +0700

    add

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,13 +6,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             bool content ;
+            int input;
             Random random = new Random();
-            int randomNumber = random.Next(0, 9);
-            int val1 = Convert.ToInt32(Console.ReadLine());  
+            int randomNumber = random.Next(0, 9);           
             Console.WriteLine(randomNumber);
-            content = true;
-            Console.WriteLine("",content);
-
+            Console.Write("Enter Input : ");
+            int.TryParse(Console.ReadLine(), out input);
+            bool A = true;
+            if (A)
+            {
+                Console.WriteLine("input={0} random={1}",input, randomNumber);
+            }
             Console.ReadKey();
         }
     }
